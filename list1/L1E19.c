e5d2cb1798b6786bd252668f2dc5888e3f41dd4d
Diego Martinez <diegomrz@outlook.com>
2019-02-21 00:50:46 -0300

    Updating exercise 19

--- list1/L1E19.c
+++ list1/L1E19.c
@@ -1,4 +1,4 @@
-* ///////////////////////////////////////
+/* ///////////////////////////////////////
 ///////////////////////////////////////
  19) Receba tres numeros que garantam a existencia de uma equacao completa do segundo grau. 
 Se existirem rai'zes reais exiba-as e informe ao usuario se sao iguais ou diferentes. 
