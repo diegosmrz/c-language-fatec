bc36e3be2aed6dc734d1c42168f7e7d5e406e944
Diego Martinez <diegomrz@outlook.com>
2019-03-03 12:49:43 -0300

    Adding base to Exec 17
    Adding first base do exec 17, it still needs to be completed.

--- list1/L1E17.c
+++ list1/L1E17.c
@@ -9,3 +9,28 @@ b) Delta maior que zero: a equacao possui duas rai'zes reais e distintas.
 c) Delta igual a  zero: a equacao possui duas rai'zes iguais.
 d) Delta menor que zero: a equacao nao apresenta rai'zes reais.
 C CODE*/ 
+
+#include <stdio.h>
+#include <math.h>
+float a, b, c = ;
+float delta, x1, x2 = 0;
+int main (){
+  
+  do(){
+    printf("\Digite o valor de A\n");
+    scanf("%f\n", &a);
+    if(a==0){ printf("\O valor de A não pode ser igual a zero."); }
+  }while(a==0);
+  
+    printf("\Digite o valor de B\n");
+    scanf("%f\n", &b);
+  
+    printf("\Digite o valor de C\n");
+    scanf("%f\n", &c);
+  
+  delta = b*b - 4*a*c;
+  
+  x1 = (-b + sqrt(delta)) / 2*a;
+  x2 = (-b - sqrt(delta)) / 2*a;
+ 
+}	
