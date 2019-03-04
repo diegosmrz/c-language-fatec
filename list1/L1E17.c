40decba0800036d6ac9350227c0da2a973002b1c
Diego Martinez <diegomrz@outlook.com>
2019-03-04 16:46:02 -0300

    Adding conditions to 17

--- list1/L1E17.c
+++ list1/L1E17.c
@@ -17,15 +17,15 @@ float delta, x1, x2 = 0;
 int main (){
   
   do(){
-    printf("\Digite o valor de A\n");
+    printf("Digite o valor de A\n");
     scanf("%f\n", &a);
     if(a==0){ printf("\O valor de A não pode ser igual a zero."); }
   }while(a==0);
   
-    printf("\Digite o valor de B\n");
+    printf("Digite o valor de B\n");
     scanf("%f\n", &b);
   
-    printf("\Digite o valor de C\n");
+    printf("Digite o valor de C\n");
     scanf("%f\n", &c);
   
   delta = b*b - 4*a*c;
@@ -33,4 +33,13 @@ int main (){
   x1 = (-b + sqrt(delta)) / 2*a;
   x2 = (-b - sqrt(delta)) / 2*a;
  
+  if(delta > 0){
+    //Delta maior que zero: a equacao possui duas rai'zes reais e distintas.
+  }
+  else if(delta == 0){
+    //Delta maior que zero: a equacao possui duas rai'zes reais e distintas.
+  }
+  else{
+   //Delta menor que zero: a equacao nao apresenta rai'zes reais.
+  }
 }	
