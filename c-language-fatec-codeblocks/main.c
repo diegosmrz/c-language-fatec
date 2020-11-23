843e965c1e2f75cb18b3b3d84ebbb891d30bd439
diegomrz <diegomrz@outlook.com>
2020-11-23 01:26:42 -0300

    Adicionando L3 07

--- c-language-fatec-codeblocks/main.c
+++ c-language-fatec-codeblocks/main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int num1,mod=0;
+float result;
 
+int main(){
+    printf("Digite a quantidade de numero pares: \n");
+    scanf("%d", &num1);
+    num1 = num1*2;
+
+    while(num1 != 0){
+        num1--;
+        mod = num1%2;
+        if(mod == 0){
+            printf("\n %d numero PAR.", num1);
+        }
+            else if (mod != 0) {
+                printf("\n %d numero IMPAR.", num1);
+            }
+    }
+}
--- list3/L3E07.c
+++ list3/L3E07.c
@@ -1,2 +1,21 @@
-/* 7)	Leia um número inteiro representado pela variável N até que N seja igual a zero. Exiba uma mensagem informando se o número é par ou ímpar.
+/* 7)	Leia um número inteiro representado pela variável N até que N seja igual a zero. 
+Exiba uma mensagem informando se o número é par ou ímpar. */
 
+#include <stdio.h>
+int num1,mod=0;
+float result;
+
+int main(){
+    printf("Digite a quantidade de numero pares: \n");
+    scanf("%d", &num1);
+    while(num1 != 0){
+        num1--;
+        mod = num1%2;
+        if(mod == 0){
+            printf("\n %d numero PAR.", num1);
+        }
+            else if (mod != 0) {
+                printf("\n %d numero IMPAR.", num1);
+            }
+    }
+}
