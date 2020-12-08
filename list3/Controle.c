cbaea64b0433c381e929d86663171669d9759bc7
diegomrz <diegomrz@outlook.com>
2020-12-08 03:11:32 -0300

    Adicionando controle

---
+++ list3/Controle.c
@@ -0,0 +1,19 @@
+/* Controle de Exercícios */
+E01 - OK
+E02 - OK
+E03 - OK
+E04 - OK
+E05 - OK
+E06 - OK
+E07 - OK
+E08 - OK
+E09 - NÃO
+E10 - NÃO
+E11 - NÃO
+E12 - OK
+E13 - NÃO
+E14 - NÃO
+E15 - OK
+E16 - OK
+E17 - NÃO
+E18 - NÃO
