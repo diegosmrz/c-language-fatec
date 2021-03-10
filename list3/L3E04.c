87fecd1c8b3937989918a95a9d3f7bd547c3515d
diegomrz <diegomrz@outlook.com>
2021-03-10 02:50:58 -0300

    Ajuste exercícios 3 e 4

--- list3/L3E04.c
+++ list3/L3E04.c
@@ -1,6 +1,7 @@
 /* 4)	Pergunte ao usuário quantos alunos tem na sala dele. 
 Em seguida, através de um laço do while, peça ao usuário para que entre com as notas de todos os alunos da sala,
-um por vez. Exiba a média da turma e quantas notas foram digitadas maiores ou iguais a 6.  */
+um por vez. 
+Exiba a média da turma e quantas notas foram digitadas maiores ou iguais a 6.  */
 
 #include <stdio.h>
 void main (){
@@ -9,7 +10,7 @@ void main (){
 		printf("\nDigite quantos alunos tem na sua turma: \n");
 		scanf("%d", &qtAlunos);
 	
-	do{
+	while(vez<qtAlunos){
 		printf("\nDigite a nota da pessoa: \n");
 		scanf("%d", &nota);
 		somaNotas += nota;
@@ -17,7 +18,7 @@ void main (){
 			contaNotas++;
 		}
 		vez++;
-	}while(vez<qtAlunos);
+	};
 
 	printf("\nMédia de notas : %d", somaNotas/qtAlunos);
 	printf("\nQuantidade de notas maiores que 6 : %d", contaNotas);
